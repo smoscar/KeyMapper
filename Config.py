89849afcb97c81a19ff6fe07e23055b91e749639
Oscar Merlos <oscar.merlos.arias@gmail.com>
2019-09-28 07:53:50 -0700

    Handles stored data in JSON objects

--- Config.py
+++ Config.py
@@ -1,14 +1,33 @@
+import json
 
 class Config:
     """docstring for Config"""
     def __init__(self, file="./config.cfg"):
         self.path = file
+        
+    def getContents(self):
+        config = open(self.path, "r")
+        jsonMap = config.read()
+        contents = {} if jsonMap == '' else json.loads(jsonMap)
+        config.close()
+        return contents
+        
+    def setContents(self, content):
+        config = open(self.path, "w")
+        config.seek(0)
+        config.write(json.dumps(content))
+        config.close()
     
-    def addMapping(self, map):
+    def addMapping(self, map, type='buttonToKey'):
         if len(map) <> 2:
             return False
         # Save the config
-        config = open(self.path, "w")
-        config.writelines([str(line) + "\n" for line in map])
-        config.close()
+        currentConfig = self.getContents()
+        
+        # Check for non existing types
+        if type not in currentConfig:
+            currentConfig[type] = {}
+        currentConfig[type][str(map[0])] = map[1]
+        self.setContents(currentConfig)
+        
         return True
